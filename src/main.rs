9b6af3fcc6cc966e34cb7ebab8e5d230320da89d
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2022-12-15 12:20:18 +0000

    Add create_function macro

--- src/main.rs
+++ src/main.rs
@@ -8,4 +8,15 @@ fn main() {
     }
 
     say_hello!();
+
+    macro_rules! create_function {
+        ($func_name:ident) => {
+            fn $func_name() {
+                println!("You called {:?}()", stringify!($func_name));
+            }
+        };
+    }
+
+    create_function!(test);
+    test();
 }
