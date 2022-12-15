a831125f433212ba71062ada0f5949dc3fcdf136
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2022-12-15 12:21:32 +0000

    Restructure code with macros outside main()

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
+macro_rules! say_hello {
+    () => {
+        println!("Hello!")
+    };
+}
+
+macro_rules! create_function {
+    ($func_name:ident) => {
+        fn $func_name() {
+            println!("You called {:?}()", stringify!($func_name));
+        }
+    };
+}
+
 fn main() {
-    macro_rules! say_hello {
-        () => {
-            println!("Hello!")
-        };
-    }
-
     say_hello!();
-
-    macro_rules! create_function {
-        ($func_name:ident) => {
-            fn $func_name() {
-                println!("You called {:?}()", stringify!($func_name));
-            }
-        };
-    }
 
     create_function!(test);
     test();
